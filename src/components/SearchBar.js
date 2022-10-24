f77946d7fc364271e6a6177647b8808c5a4802b0
bellafernandes <fernandes.bella13@gmail.com>
2022-10-24 20:15:00 -0300

    Added responsiveness

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -41,28 +41,28 @@ export default function SearchBar() {
 
   return (
     <>
-      <div className="pr-4 sm:pr-0 sm:w-56 ml-2 relative">
+      <div className="pr-4 sm:pr-0 sm:w-56 ml-2 relative flex justify-end">
         {/* <Icon
                 path="res-react-dash-search"
                 className="w-5 h-5 search-icon left-3 absolute"
               /> */}
-        <div class="relative">
+        <div class="relative ">
           <input
             type="search"
             value={ProductsData.name}
             onChange={filter}
             id="floating_outlined"
             className={background 
-                ? "block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-600 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                : "block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                ? "block px-2.5 pb-2.5 pt-2 text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-600 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                : "block px-2.5 pb-2.5 pt-2 text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             }
             placeholder=" "
           />
           <label
             for="floating_outlined"
             className={background 
-                ? "cursor-text absolute bg-zinc-900 text-lg text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-                : "cursor-text absolute bg-white text-lg text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"}
+                ? "cursor-text absolute bg-zinc-900 text-md text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                : "cursor-text absolute bg-white text-md text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"}
           >
             Search product
           </label>
@@ -82,8 +82,8 @@ export default function SearchBar() {
       <div
         className={
           background
-            ? "absolute mt-[580px] ml-3 shadow-lg z-50 bg-zinc-900 transtion-all duration-300"
-            : "absolute mt-[580px] ml-3 shadow-lg z-50 transtion-all duration-300"
+            ? "absolute mt-[580px] ml-12 shadow-lg z-50 bg-zinc-900 transtion-all duration-300"
+            : "absolute mt-[580px] ml-12 shadow-lg z-50 bg-white transtion-all duration-300"
         }
       >
         {/* product array with filter */}
@@ -93,7 +93,7 @@ export default function SearchBar() {
             <Link to={"/ProductPage/" + ProductsData.id} key={ProductsData.id}>
               <div
                 key={ProductsData.id}
-                className="rounded-lg mb-4 mx-2 p-4 user hover:bg-zinc-100 transition-all duration-200"
+                className="rounded-lg mb-4 mx-2 p-4 user hover:bg-zinc-300/50 transition-all duration-200"
               >
                 <div className="flex gap-2 items-center">
                   <img
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -8,20 +8,18 @@ import clsx from "https://cdn.skypack.dev/clsx@1.1.1";
 import SearchBar from "./SearchBar";
 
 import {
-  ArrowPathIcon,
   Bars3Icon,
   BookmarkSquareIcon,
   CalendarIcon,
   ChartBarIcon,
   CursorArrowRaysIcon,
   LifebuoyIcon,
-  PhoneIcon,
-  PlayIcon,
   ShieldCheckIcon,
   Squares2X2Icon,
   XMarkIcon,
   ShoppingBagIcon,
   HeartIcon,
+  UserIcon,
 } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import ProductsData from "../pages/ProductsData";
@@ -120,15 +118,15 @@ export default function TopBar() {
     <Popover
       className={
         background
-          ? "bg-zinc-900 fixed top-0 w-full z-50 transtion-all duration-300 text-zinc-200"
-          : "bg-transparent fixed top-0 w-full z-50 transtion-all duration-300 text-zinc-800"
+          ? "bg-zinc-900 px-10 fixed top-0 w-full z-50 transtion-all duration-300 text-zinc-200"
+          : "bg-transparent px-10 fixed top-0 w-full z-50 transtion-all duration-300 text-zinc-700"
       }
     >
-      <div className="mx-auto w-4/5 px-4 sm:px-6 bg">
-        <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1">
+      <div className="mx-auto w-full px-4 sm:px-6 bg">
+        <div className="flex items-center justify-between py-6 md:space-x-10">
+          <div className="flex justify-start xl:flex-1">
             <a href="/">
-              <span className="sr-only">Your Company</span>
+              {/* <span className="sr-only">Your Company</span> */}
               <img
                 className="h-8 w-auto sm:h-10"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -136,20 +134,26 @@ export default function TopBar() {
               />
             </a>
           </div>
-          <div className="-my-2 -mr-2 md:hidden">
-            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <div className="-my-2 -mr-2 lg:hidden">
+            <Popover.Button
+              className={
+                background
+                  ? "inline-flex items-center justify-center rounded-md bg-transparent p-2 text-gray-400 hover:text-gray-100 focus:outline-none transition-all duration-200"
+                  : "inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none transition-all duration-200"
+              }
+            >
               <span className="sr-only">Open menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3Icon className="h-8 w-8" aria-hidden="true" />
             </Popover.Button>
           </div>
-          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
-            <Popover className="relative">
+          <Popover.Group as="nav" className="hidden space-x-10 lg:flex">
+            <Popover className="relative hover:text-gray-500 transition-all duration-200">
               {({ open }) => (
                 <>
-                  <Popover.Button className="group inline-flex items-center rounded-md text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-0 focus:ring-offset-0">
+                  <Popover.Button className="group inline-flex items-center rounded-md text-base font-medium focus:outline-none focus:ring-0 focus:ring-offset-0">
                     <span>Products</span>
                     <ChevronDownIcon
-                      className="ml-2 h-5 w-5 group-hover:text-gray-500"
+                      className="ml-2 h-5 w-5"
                       aria-hidden="true"
                     />
                   </Popover.Button>
@@ -165,19 +169,21 @@ export default function TopBar() {
                   >
                     <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                        <div className={background
+                                        ? "relative grid gap-6 bg-zinc-900 text-zinc-200 px-5 py-6 sm:gap-8 sm:p-8"
+                                        : "relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8"}>
                           {products.map((item) => (
                             <a
                               key={item.name}
                               href={item.href}
-                              className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                              className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-200/20"
                             >
                               {/* <item.icon
                                 className="h-6 w-6 flex-shrink-0 text-indigo-600"
                                 aria-hidden="true"
                               /> */}
                               <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900">
+                                <p className="text-base font-medium">
                                   {item.name}
                                 </p>
                                 {/* <p className="mt-1 text-sm text-gray-500">
@@ -194,32 +200,32 @@ export default function TopBar() {
               )}
             </Popover>
 
-            <a href="#" className="text-base font-medium">
+            <a href="#" className="text-base font-medium hover:text-gray-500 transition-all duration-200">
               Drinks
             </a>
-            <a href="#" className="text-base font-medium">
+            <a href="#" className="text-base font-medium hover:text-gray-500 transition-all duration-200">
               News
             </a>
 
-            <a href="#" className="text-base font-medium">
+            <a href="#" className="text-base font-medium hover:text-gray-500 transition-all duration-200">
               Events
             </a>
           </Popover.Group>
-          <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-            
-
+          <div className="hidden items-center justify-end lg:flex lg:flex-1 lg:w-0">
             <div className="flex items-center gap-4 ml-6">
-              
               {/* SEARCH BAR */}
-            <SearchBar />
-              <HeartIcon className="w-8 h-8 text-zinc-800 hover:text-zinc-500 transition duration-200" />
+              <SearchBar />
               <div
                 className="shopping-cart cursor-pointer"
                 onClick={() => navigate("/cart")}
               >
                 <ShoppingBagIcon
                   id="cartIcon"
-                  className="w-8 h-8 text-zinc-800 hover:text-zinc-500 transition duration-200"
+                  className={
+                    background
+                      ? "w-8 h-8 text-zinc-400 hover:text-zinc-200 transition-all duration-200"
+                      : "w-8 h-8 text-zinc-500 hover:text-zinc-700 transition-all duration-200"
+                  }
                 />
                 <div
                   className={clsx(
@@ -233,11 +239,21 @@ export default function TopBar() {
                   </p>
                 </div>
               </div>
+              <p
+                className={
+                  background
+                    ? "ml-4 text-zinc-400 hover:text-zinc-200 transition-all duration-200 cursor-pointer"
+                    : "ml-4 text-zinc-700 hover:text-zinc-500 transition-all duration-200 cursor-pointer"
+                }
+              >
+                Sign In{" "}
+              </p>
             </div>
           </div>
         </div>
       </div>
 
+      {/* MOBILE MENU */}
       <Transition
         as={Fragment}
         enter="duration-200 ease-out"
@@ -249,80 +265,132 @@ export default function TopBar() {
       >
         <Popover.Panel
           focus
-          className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
+          className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition lg:hidden"
         >
-          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <div className={background
+            ? "divide-y-2 divide-zinc-800 text-zinc-200 text-lg rounded-lg bg-zinc-900 shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-200"
+            : "divide-y-2 divide-zinc-100 text-zinc-500 text-lg rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-200"}>
             <div className="px-5 pt-5 pb-6">
               <div className="flex items-center justify-between">
                 <div>
-                  <img
+                  {/* <img
                     className="h-8 w-auto"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                     alt="Your Company"
-                  />
+                  /> */}
                 </div>
-                <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <div className="-mr-2 px-10">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none">
                     <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="h-8 w-8" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
-              <div className="mt-6">
+              <div className="mt-6 px-10">
                 <nav className="grid gap-y-8">
-                  {products.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
-                    >
-                      <item.icon
-                        className="h-6 w-6 flex-shrink-0 text-indigo-600"
-                        aria-hidden="true"
-                      />
-                      <span className="ml-3 text-base font-medium text-gray-900">
-                        {item.name}
-                      </span>
-                    </a>
-                  ))}
+                  <Popover className="relative">
+                    {({ open }) => (
+                      <>
+                        <Popover.Button className="group inline-flex items-center rounded-md hover:text-zinc-400/80 transition-all duration-200 focus:outline-none focus:ring-0 focus:ring-offset-0">
+                          <span>Products</span>
+                          <ChevronDownIcon
+                            className="ml-2 h-5 w-5"
+                            aria-hidden="true"
+                          />
+                        </Popover.Button>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-200"
+                          enterFrom="opacity-0 translate-y-1"
+                          enterTo="opacity-100 translate-y-0"
+                          leave="transition ease-in duration-150"
+                          leaveFrom="opacity-100 translate-y-0"
+                          leaveTo="opacity-0 translate-y-1"
+                        >
+                          <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
+                            <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                              <div className={background
+                                ? "relative grid gap-6 bg-zinc-800 px-5 py-6 sm:gap-8 sm:p-8"
+                                : "relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8"
+                                }>
+                                {products.map((item) => (
+                                  <a
+                                    key={item.name}
+                                    href={item.href}
+                                    className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-200/20"
+                                  >
+                                    {/* <item.icon
+                                className="h-6 w-6 flex-shrink-0 text-indigo-600"
+                                aria-hidden="true"
+                              /> */}
+                                    <div className="ml-4">
+                                      <p className="">
+                                        {item.name}
+                                      </p>
+                                      {/* <p className="mt-1 text-sm text-gray-500">
+                                  {item.description}
+                                </p> */}
+                                    </div>
+                                  </a>
+                                ))}
+                              </div>
+                            </div>
+                          </Popover.Panel>
+                        </Transition>
+                      </>
+                    )}
+                  </Popover>
+
+                  <a href="#" className="hover:text-zinc-400/80 transition-all duration-200">
+                    Drinks
+                  </a>
+                  <a href="#" className="hover:text-zinc-400/80 transition-all duration-200">
+                    News
+                  </a>
+
+                  <a href="#" className="hover:text-zinc-400/80 transition-all duration-200">
+                    Events
+                  </a>
                 </nav>
               </div>
             </div>
-            <div className="space-y-6 py-6 px-5">
-              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+            <div className="space-y-6 py-6 px-5 flex justify-center gap-4">
+              <div
+                className="shopping-cart cursor-pointer mx-auto mt-[4%]"
+                onClick={() => navigate("/cart")}
+              >
+                <ShoppingBagIcon
+                  id="cartIcon"
+                  className={
+                    background
+                      ? "w-10 h-10 text-zinc-400 hover:text-zinc-200 transition-all duration-200"
+                      : "w-10 h-10 text-zinc-500 hover:text-zinc-700 transition-all duration-200"
+                  }
+                />
+                <div
+                  className={clsx(
+                    getTotalQuantity() > "0"
+                      ? "transition-all duration-200 block absolute rounded-full w-6 h-6 bg-sky-500 -mt-12 ml-6"
+                      : "hidden transition-all duration-200"
+                  )}
                 >
-                  Pricing
-                </a>
-
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  Docs
-                </a>
-                {resources.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    {item.name}
-                  </a>
-                ))}
+                  <p className="-mt-0.5 ml-2 mx-auto text-zinc-200 font-semibold">
+                    {getTotalQuantity() || 0}
+                  </p>
+                </div>
               </div>
-              <div>
+
+              <div className="mx-auto">
                 <a
                   href="#"
-                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-zinc-800 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-zinc-700 transition-all duration-200"
                 >
                   Sign up
                 </a>
                 <p className="mt-6 text-center text-base font-medium text-gray-500">
                   Existing customer?{" "}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                  <a href="#" className="text-sky-600 hover:text-sky-500">
                     Sign in
                   </a>
                 </p>
